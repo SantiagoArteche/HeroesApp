53eff3eb2aa27050246ba807ded151fce138ba7f
santiago arteche <santiarteche@hotmail.com>
2024-02-19 03:40:10 -0300

    vercel.json added

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import HeroesApp from "./HeroesApp.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <HeroesApp />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<HeroesApp />);
